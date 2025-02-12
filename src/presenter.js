6ba09b6b73bc8b4eeb1be78ede07aa051891c755
Luis Felipe Velasco Lopez <luis.velasco@ucb.edu.bo>
2025-02-12 18:10:43 -0400

    ultima version lista la multiplicacion

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,30 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const sumForm = document.querySelector("#sumar-form");
+const sumFirst = document.querySelector("#primer-numero-suma");
+const sumSecond = document.querySelector("#segundo-numero-suma");
+const sumDiv = document.querySelector("#resultado-suma");
 
-form.addEventListener("submit", (event) => {
+const multForm = document.querySelector("#multiplicar-form");
+const multFirst = document.querySelector("#primer-numero-mult");
+const multSecond = document.querySelector("#segundo-numero-mult");
+const multDiv = document.querySelector("#resultado-multiplicacion");
+
+sumForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const firstNumber = Number(sumFirst.value); 
+  const secondNumber = Number(sumSecond.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  sumDiv.innerHTML = "<p>La suma es: " + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+multForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number(multFirst.value); 
+  const secondNumber = Number(multSecond.value);
+
+  multDiv.innerHTML = "<p>La multiplicación es: " + multiplicar(firstNumber, secondNumber) + "</p>";
 });
